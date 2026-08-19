bd052d1b6bb1d8117dd665b19c757195c0abd93a
mahmood <mahmood@gmail.com>
2026-08-19 05:11:28 -0700

    Fix vite base and yaml syntax

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// نام مخزن خود را اینجا وارد کنید (اگر مخزن شما taskManager است، همین را نگه دارید)
+const REPO_NAME = '/taskManager/'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/taskManager/',
+  base: process.env.NODE_ENV === 'production' ? REPO_NAME : '/',
 })
