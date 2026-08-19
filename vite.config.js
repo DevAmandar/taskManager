ac76025940f5ab313061e2a08da8e42bfa4ee940
mahmood <mahmood@gmail.com>
2026-08-19 05:32:17 -0700

    Fix final deploy with GitHub Actions

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// نام مخزن خود را اینجا وارد کنید (اگر مخزن شما taskManager است، همین را نگه دارید)
-const REPO_NAME = '/taskManager/'
-
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.NODE_ENV === 'production' ? REPO_NAME : '/',
+  base: '/taskManager/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        404: './404.html',
+      },
+    },
+  },
 })
