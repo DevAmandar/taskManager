96e76d0c3c096455368cc3e138d851391aa85624
mahmood <mahmood@gmail.com>
2026-08-19 03:27:55 -0700

    debug

--- src/layout/TasksPage.tsx
+++ src/layout/TasksPage.tsx
@@ -1,5 +1,5 @@
 import { ReactNode, useContext, useRef } from "react";
-import Task from "../component/task/Task";
+import Task from "../Component/Task/Task";
 import Modal from "../modal/Modal";
 import CreateTaskModal from "../modal/CreateTaskModal/CreateTaskModal";
 import { BoardContext } from "../context/BoardContext";
